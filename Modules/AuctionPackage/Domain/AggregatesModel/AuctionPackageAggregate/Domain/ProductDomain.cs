35f32fd79e11a91d286a37ca8c2b08589fe7ab97
hoinx <hoinx01@gmail.com>
2018-09-04 17:41:25 +0700

    up

---
+++ Modules/AuctionPackage/Domain/AggregatesModel/AuctionPackageAggregate/Domain/ProductDomain.cs
@@ -0,0 +1,16 @@
+﻿using Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.AggregatesModel.AuctionPackageAggregate.Domain
+{
+    public class ProductDomain : Subdomain
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string BrandId { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Modules/AuctionPackage/Domain/AggregatesModel/AuctionPackageAggregate/Domain/VariantDomain.cs
@@ -0,0 +1,16 @@
+﻿using Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.AggregatesModel.AuctionPackageAggregate.Domain
+{
+    public class VariantDomain : Subdomain
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal BasePrice { get; set; }
+
+    }
+}
